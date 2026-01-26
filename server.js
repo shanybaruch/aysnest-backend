85d4972f6e0d95b354e83a68224e2f620ec54b16
Shany Baruch <baruchshany@gmail.com>
2026-01-26 21:49:20 +0200

    Add url from Render

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV === 'production') {
         origin: [   'http://127.0.0.1:3000',
                     'http://localhost:3000',
                     'http://127.0.0.1:5173',
-                    'http://localhost:5173'
+                    'http://localhost:5173',
+                    'https://aysnest-frontend-react.onrender.com'
                 ],
         credentials: true
     }
